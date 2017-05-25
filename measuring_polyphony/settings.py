7ec3b3b8630ea82902aff4c334f8d4561fb263ed
Arya Boudaie <aboudaie@brandeis.edu>
2017-05-25 19:33:15 -0400

    Changed DB to PSQL

--- measuring_polyphony/settings.py
+++ measuring_polyphony/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'measuring_polyphony.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polyphony',
+        'USER': 'polyphony',
+        'PASSWORD': secrets.DATABASE_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
