90d38d382fbf968651bd9e3c32fa435144b0f40a
Arya Boudaie <aboudaie@brandeis.edu>
2017-05-30 14:02:49 -0500

    Add localhost to settings.py ALLOWED_HOST

--- measuring_polyphony/settings.py
+++ measuring_polyphony/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = secrets.debug
 
-ALLOWED_HOSTS = ['174.138.49.237', '127.0.0.1']
+ALLOWED_HOSTS = ['174.138.49.237', '127.0.0.1', 'localhost']
 
 
 # Application definition
