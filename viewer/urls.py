3e67453aee746087949c68a95f2c79726d76976f
Arya Boudaie <aboudaie@brandeis.edu>
2017-05-30 09:40:52 -0500

    Added template

--- viewer/urls.py
+++ viewer/urls.py
@@ -7,8 +7,8 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello),
+    url(r'^$', views.hello, name='index'),
     url(r'^compositions$', views.CompositionListView.as_view(), name='composition-list'),
     url(r'^compositions/(?P<pk>\d+)/$', views.CompositionDetailView.as_view(), name='composition-detail'),
     url(r'^compositions/(?P<pk>\d+)/iiif$', views.iiif, name='composition-iiif'),
-]+ static("media", document_root=settings.MEDIA_ROOT)
+]+ static("media", document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
